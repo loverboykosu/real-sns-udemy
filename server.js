17d95307bd753bbdbeb3811e85084d2794c104f2
loverboy <loverboykosu@gmail.com>
2025-06-19 11:20:02 +0900

    feat: #10 lecture add express middleware users.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const userRoute = require("./routes/users.js");
 
+//ミドルウェア
+app.use("/api/users", userRoute);
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello express");
 });
 
 app.listen(PORT, () => console.log("Start server"));
