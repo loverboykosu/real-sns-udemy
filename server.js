dfee050b105f06775bc0431fa0622794da99cec4
loverboy <loverboykosu@gmail.com>
2025-06-19 11:22:38 +0900

    feat: #11 lecture add auth.js

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const userRoute = require("./routes/users.js");
-
+const authRoute = require("./routes/auth");
 //ミドルウェア
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 app.get("/", (req, res) => {
   res.send("hello express");
 });
