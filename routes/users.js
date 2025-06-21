32c56ad105b6e383b954c9b247b79801b02236ff
loverboy <loverboykosu@gmail.com>
2025-06-21 19:14:35 +0900

    feat: #21 lecture add delete method

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,40 @@
+const User = require("../models/User");
+
 const router = require("express").Router();
 
+//CRUD操作
+//C : Create
+//R : Read
+//U : Update
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("ユーザ情報が更新されました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("あなたは自分のアカウントの時だけ情報を更新できます");
+  }
+});
+//D : Delete
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("ユーザ情報が削除されました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("あなたは自分のアカウントの時だけ情報を削除できます");
+  }
+});
 module.exports = router;
