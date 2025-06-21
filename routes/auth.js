c20ddacd26694b395deabd748edc586c2286c41f
loverboy <loverboykosu@gmail.com>
2025-06-21 18:34:06 +0900

    feat: #19 lecture add login func

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { RiErrorWarningFill } = require("react-icons/ri");
 const User = require("../models/User");
 //ユーザ登録
 router.post("/register", async (req, res) => {
@@ -9,7 +10,22 @@ router.post("/register", async (req, res) => {
       password: req.body.password,
     });
     const user = await newUser.save();
-    return req.status(200).json(BiUserCircle);
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//ログイン
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(404).send("ユーザが見つかりません");
+    const vailedPassword = req.body.password === user.password;
+    //パスワードが不正な場合
+    if (!vailedPassword) return res.status(400).send("不正なパスワードです");
+    //パスワードが正しい時
+    return res.status(200).json(user);
   } catch (err) {
     return res.status(500).json(err);
   }
