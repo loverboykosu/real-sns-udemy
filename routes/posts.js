628ad327a3b5dcbd557751cfce084e5618d86f70
loverboy <loverboykosu@gmail.com>
2025-06-22 20:50:38 +0900

    #31 lecture add like method

--- routes/posts.js
+++ routes/posts.js
@@ -57,4 +57,32 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+//特定の投稿にいいねをおす
+outer.put("/:id/like", async (req, res) => {
+  //follow対象が自分のアカウントでないかどうか
+  //body.userId : 自分のアカウント、params.id : follow対象のアカウント
+  try {
+    //user : follow対象の情報
+    const post = await Post.findById(req.params.id);
+    //投稿にいいねをしていない場合
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({
+        $push: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(200).json("like成功");
+    } else {
+      await post.updateOne({
+        $pull: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(403).json("unlike成功");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 module.exports = router;
