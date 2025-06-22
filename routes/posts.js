ac307a408919f6be9f0c08d83c2268bb953ba44e
loverboy <loverboykosu@gmail.com>
2025-06-22 11:35:03 +0900

    #28 lecture add edit post method

--- routes/posts.js
+++ routes/posts.js
@@ -12,4 +12,23 @@ router.post("/", async (req, res) => {
   }
 });
 
+//投稿を更新
+router.put("/:id", async (req, res) => {
+  //body.userId : post id
+  //params id : 目的のpost id
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json("投稿編集に成功");
+    } else {
+      return res.status(403).json("あなたは他の人の投稿を編集できません");
+    }
+  } catch (err) {
+    return res.status(403).json(err);
+  }
+});
+
 module.exports = router;
