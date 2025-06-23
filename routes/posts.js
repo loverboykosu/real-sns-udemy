6742bc9017c8ea5770d9e55af5caa7b2d4c4cd1a
loverboy <loverboykosu@gmail.com>
2025-06-23 21:09:37 +0900

    feat: #33 lecture add get timeline method

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
-
+const User = require("../models/User");
 //投稿を作成
 router.post("/", async (req, res) => {
   const newPost = new Post(req.body);
@@ -80,6 +80,24 @@ router.put("/:id/like", async (req, res) => {
       });
       return res.status(403).json("unlike成功");
     }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//タイムラインの投稿を取得
+//timeline/all : timelineだけだと、/:idと区別がつかないため
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    //followingsの投稿内容を全て取得する
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    return res.status(200).json(userPosts.concat(...friendPosts));
   } catch (err) {
     return res.status(500).json(err);
   }
